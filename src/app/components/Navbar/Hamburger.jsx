6b6eceff5d427a2b7b0b7c4ce3b8366fae82edf2
Damian Aung <damianaung@Damians-MacBook-Air.local>
2024-11-05 01:12:39 +0700

    navbar

---
+++ src/app/components/Navbar/Hamburger.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Hamburger = ({ containerStyles = "border-white", open }) => {
+  return (
+    <div className="w-[30px] flex gap-2 flex-col cursor-pointer">
+      <div
+        id="top"
+        className={`border-b-[2px] ${containerStyles} w-full h-[2px]`}
+      ></div>
+      <div
+        id="middle"
+        className={`border-b-[2px] opacity-100 ${containerStyles} w-full h-[2px]`}
+      ></div>
+      <div
+        id="bottom"
+        className={`border-b-[2px] ${containerStyles} w-full h-[2px]`}
+      ></div>
+    </div>
+  );
+};
+
+export default Hamburger;
--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import Link from "next/link";
-import { useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Hamburger from "./Hamburger";
 
 const navLinks = [
   { title: "Explore", url: "#explore" },
@@ -13,38 +15,51 @@ const navLinks = [
 
 const Navbar = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const comp = useRef(null);
+  const container = useRef();
+  const t1 = useRef();
 
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      const t1 = gsap.timeline();
+  const { contextSafe } = useGSAP({ scope: container });
 
-      t1.to("#dropdown-nav", {
+  useEffect(() => {
+    t1.current = gsap.timeline({ paused: true });
+    t1.current
+      .to("#dropdown-nav", {
         y: "0",
         duration: 0.5,
         delay: 0.4,
         ease: "power2.in",
-      }).from("#nav-title", {
+      })
+      .from("#nav-title", {
         opacity: 0,
         stagger: 0.1,
       });
-    }, comp);
+  }, []);
 
-    return () => ctx.revert();
-  }, [isDropdownOpen]);
+  const toggleDropdown = contextSafe(() => {
+    if (!isDropdownOpen) {
+      t1.current.play();
+    } else {
+      t1.current.reverse();
+    }
+    setDropdownOpen(!isDropdownOpen);
+  });
 
   return (
-    <nav ref={comp}>
+    <nav ref={container}>
       <div className="bg-transparent absolute my-3 top-0 left-0 z-40 w-full">
         <div className="container mx-auto flex justify-between w-full items-center">
           <div className="select-none invisible md:visible text-white font-primary uppercase font-bold text-accent text-3xl">
             Urban.
           </div>
-          <button
-            className="text-white"
-            onClick={() => setDropdownOpen(!isDropdownOpen)}
-          >
-            Menu
+          <button className="text-white" onClick={toggleDropdown}>
+            <Hamburger
+              containerStyles={
+                isDropdownOpen
+                  ? "border-accent transition-all ease-in-out duration-500 delay-300"
+                  : "border-white transition-all ease-in-out duration-500 delay-300"
+              }
+              open={isDropdownOpen}
+            />
           </button>
         </div>
       </div>
@@ -56,7 +71,7 @@ const Navbar = () => {
           {navLinks.map(({ title, url }, index) => {
             return (
               <Link
-                className="cursor-pointer"
+                className="cursor-pointer hover:text-soft"
                 id="nav-title"
                 href={url}
                 key={index}
