003e6ef85a83e4376910bde65bca7ef676a30826
Damian Aung <damianaung@Damians-MacBook-Air.local>
2024-11-12 15:03:11 +0700

    added mobile view

--- src/app/components/About/About.jsx
+++ src/app/components/About/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
             line,
             { height: "0vh" },
             {
-              height: "35vh",
+              height: "30vh",
               ease: "power1.in",
             },
             ">1"
@@ -91,69 +91,71 @@ const About = () => {
 
   return (
     <section
-      className="font-primary bg-white text-black overflow-hidden"
+      className="font-primary bg-white flex flex-col gap-10 lg:gap-0 text-black overflow-hidden my-20"
       ref={containerRef}
     >
-      <div>
-        {journeyContent.map((item, index) => (
+      {journeyContent.map((item, index) => (
+        <div
+          key={index}
+          ref={(el) => (sectionRefs.current[index] = el)}
+          className="grid md:grid-cols-[60px_1fr] mx-20 lg:mx-0 lg:grid-cols-[1fr_80px_1fr] place-items-center opacity-0"
+        >
+          {/* Left Content */}
           <div
-            key={index}
-            ref={(el) => (sectionRefs.current[index] = el)} // Store reference for each section
-            className="grid grid-cols-[1fr_80px_1fr] place-items-center opacity-0" // Start with opacity 0
+            className={`hidden lg:flex items-center ${
+              item.placement === "right" && "invisible"
+            }`}
           >
-            <div
-              className={`flex items-center ${
-                item.placement === "right" && "invisible"
-              }`}
-            >
-              <article className="max-w-[300px] text-right">
-                <strong className="text-lg">{item.title}</strong>
-                <p>{item.description}</p>
-              </article>
-              <div className="w-[300px] aspect-square relative">
-                <Image
-                  className="object-cover"
-                  quality={100}
-                  fill
-                  alt={item.imageAlt}
-                  src={item.imageSrc}
-                />
-              </div>
-            </div>
-
-            {/* Middle Line */}
-            <div className="relative">
-              <div
-                id="line"
-                className={`absolute left-[50%] translate-x-[-50%] top-[100%] border-r-[3px] border-black border-solid`}
-              ></div>
-              <div className="mx-3 grid place-items-center font-primary aspect-square text-2xl w-10 h-10 bg-black rounded-full text-white">
-                {index + 1}
-              </div>
-            </div>
-
-            <div
-              className={`flex items-center ${
-                item.placement === "left" && "invisible"
-              }`}
-            >
-              <div className="w-[300px] aspect-square relative">
-                <Image
-                  className="object-cover"
-                  quality={100}
-                  fill
-                  alt={item.imageAlt}
-                  src={item.imageSrc}
-                />
-              </div>
-              <article className="max-w-[300px]">
-                <strong className="text-lg">{item.title}</strong>
-                <p>{item.description}</p>
-              </article>
+            <article className="max-w-[300px] text-right">
+              <strong className="text-lg">{item.title}</strong>
+              <p>{item.description}</p>
+            </article>
+            <div className="w-[300px] aspect-square relative">
+              <Image
+                className="object-cover"
+                quality={100}
+                fill
+                alt={item.imageAlt}
+                src={item.imageSrc}
+              />
             </div>
           </div>
-        ))}
-      </div>
+
+          {/* Middle Line */}
+          <div className="relative md:block hidden">
+            {index + 1 !== 4 && (
+              <div
+                id="line"
+                className={`absolute left-[50%] translate-x-[-50%] top-[120%] border-r-[3px] border-black border-solid`}
+              ></div>
+            )}
+            <div className="mx-3 grid place-items-center font-primary aspect-square text-2xl w-10 h-10 bg-black rounded-full text-white">
+              {index + 1}
+            </div>
+          </div>
+
+          {/* Right Content */}
+          <div
+            className={`flex flex-col md:flex-row items-center ${
+              item.placement === "left" && "visible lg:invisible"
+            }`}
+          >
+            <div className="w-[300px] aspect-square relative">
+              <Image
+                className="object-cover"
+                quality={100}
+                fill
+                alt={item.imageAlt}
+                src={item.imageSrc}
+              />
+            </div>
+            <article className="max-w-[300px]">
+              <strong className="text-lg">{item.title}</strong>
+              <p>{item.description}</p>
+            </article>
+          </div>
+        </div>
+      ))}
     </section>
   );
 };
